21a00fda24005dfe58fd4c4a42b9ba755d417d84
mumer2 <umermalik8280@gmail.com>
2025-08-17 10:19:38 +0500

    mongo

--- netlify/functions/mongo.js
+++ netlify/functions/mongo.js
@@ -1,25 +1,15 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 
 let client;
 let db;
 
-const uri = process.env.MONGO_URI; // set in Netlify env vars
+const uri = process.env.MONGO_URI; // Make sure this is set in Netlify
 
-async function connectToDB() {
+export async function connectToDB() {
   if (db) return { client, db };
 
-  if (!uri) {
-    throw new Error("MONGO_URI is not set in environment variables.");
-  }
-
-  client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
+  client = new MongoClient(uri); // ✅ clean, no deprecated options
   await client.connect();
-  db = client.db("feshlo"); // database name
+  db = client.db("feshlo"); // use your DB name
   return { client, db };
 }
-
-module.exports = { connectToDB };
